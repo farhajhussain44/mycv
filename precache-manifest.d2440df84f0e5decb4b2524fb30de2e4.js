95081f594ecb3260068c68403ee4d307f3acd5f3
farhajhussain44 <farhajhussain29@gmail.com>
2020-02-25 23:29:28 +0530

    Updates

---
+++ precache-manifest.d2440df84f0e5decb4b2524fb30de2e4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "218b98475ff3f0b7506f0973db1734c5",
+    "url": "/mycv/index.html"
+  },
+  {
+    "revision": "19fd81b529f0aa920819",
+    "url": "/mycv/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "2892d89f8c3420730d02",
+    "url": "/mycv/static/css/main.398925f1.chunk.css"
+  },
+  {
+    "revision": "19fd81b529f0aa920819",
+    "url": "/mycv/static/js/2.ab032df0.chunk.js"
+  },
+  {
+    "revision": "6e498969aafde287c0e2e1a721426fda",
+    "url": "/mycv/static/js/2.ab032df0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2892d89f8c3420730d02",
+    "url": "/mycv/static/js/main.bfaf72d1.chunk.js"
+  },
+  {
+    "revision": "88751095c8416bab6972",
+    "url": "/mycv/static/js/runtime-main.1a7faf89.js"
+  },
+  {
+    "revision": "e569711613d0a178dedc243f554e0625",
+    "url": "/mycv/static/media/bang.e5697116.png"
+  },
+  {
+    "revision": "3c371c80a377c0e71ab4e79e35aa290e",
+    "url": "/mycv/static/media/d2poker.3c371c80.png"
+  },
+  {
+    "revision": "278728b2cc95de946bd13860f657ce93",
+    "url": "/mycv/static/media/mee.278728b2.jpg"
+  },
+  {
+    "revision": "e612600b6447b0408b98c75f32c52e2a",
+    "url": "/mycv/static/media/rohhat.e612600b.png"
+  },
+  {
+    "revision": "6d635ac3fd23feb9bdf5c63297e1cd1f",
+    "url": "/mycv/static/media/skinscase.6d635ac3.png"
+  }
+]);
